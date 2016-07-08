00879de6932ac8a5a852cc71df9c172f2087dff7
fmxustc <fmxustc@mail.ustc.edu.cn>
2016-07-08 11:21:58 +0800

    mask_pollutions version

--- GalacticStructuralParameters/analysis.py
+++ GalacticStructuralParameters/analysis.py
@@ -3,12 +3,34 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 import PIL.Image as pi
+import subprocess
+
 
 sns.set_style('white')
+init1 = pd.read_csv('init1.csv').ix[:784]
 init2 = pd.read_csv('init2.csv').ix[:784]
-sample = init2[init2.INIT > 0.3]
-# sample['index'] = range(len(sample))
-print(sample.drop_duplicates(['NAME2']))
-# for fit in sample.drop_duplicates(['NAME2']).NAME2.values:
-#     pi.open('/Users/franky/Desktop/templates/init/%s.jpg' % fit).show()
+log1 = pd.read_csv('log1.csv').ix[:784]
+log2 = pd.read_csv('log2.csv').ix[:784]
+sample1 = log1[(log1.LOG > 0.2) & (log1.LOG < 1)]
+sample2 = log2[(log2.LOG > 0.2) & (log2.LOG < 1)]
+bins = np.linspace(0.0, 1, 51)
+print(len(sample1), len(sample2))
+# print(len(sample1[sample1.INIT > 0.25]), len(sample2[sample2.INIT > 0.25]))
 
+# sns.distplot(log1['LOG'], hist=True, color='r', kde=False, bins=bins)
+# sns.distplot(init1['INIT'], hist=True, color='b', kde=False, bins=bins)
+# sns.distplot(init2['INIT'], hist=True, kde=False, bins=bins, color='r')
+# sns.distplot(log2['LOG'], hist=True, kde=False, bins=bins)
+# plt.show()
+
+ds9 = '/Applications/SAOImage\ DS9.app/Contents/MacOS/ds9'
+cmd = '-log -zoom to fit -minmax -invert -cmap value 1.75 0.5'
+ct = sample1
+n = len(ct)
+ct.index = range(n)
+print(ct)
+for i in range(1):
+    objs = ''
+    for j in range(0, n, 5):
+        objs += '/Users/franky/Desktop/type1/image/%s_r.fits ' % ct.ix[j]['NAME1']
+    subprocess.Popen('%s %s %s' % (ds9, cmd, objs), executable='/bin/zsh', shell=True)
--- GalacticStructuralParameters/operations.py
+++ GalacticStructuralParameters/operations.py
@@ -34,24 +34,31 @@ def log(gal, exponent=1000):
 
 
 def adapt_hist(gal):
-    return eps.equalize_adapthist(norm(gal), nbins=2 ** 14, kernel_size=2 ** 7, clip_limit=0.01)
+    return eps.equalize_adapthist(norm(gal), nbins=512, kernel_size=32, clip_limit=0.1)
 
 
 def truncate(init_data, seg_data, cat_data, py, px):
     gal_flag = seg_data[py][px]
-    one = np.ones_like(seg_data, dtype=bool)
-    zero = np.zeros_like(seg_data, dtype=bool)
-    pollutoin = np.zeros_like(seg_data)
-    for i in np.arange(py-150, py+151):
-        for j in np.arange(px-150, px+151):
-            s = seg_data[i][j]
+    real_galaxy = mph.remove_small_objects(np.where(seg_data == gal_flag, np.ones_like(seg_data, dtype=bool), np.zeros_like(seg_data, dtype=bool)),
+                                           connectivity=2, min_size=100)
+    y, x = np.where(real_galaxy)
+    radius = np.max(np.sqrt((y - py) ** 2 + (x - px) ** 2))
+    init_tr = tr(init_data, py, px, radius)
+    seg_tr = tr(seg_data, py, px, radius)
+    poll_tr = np.zeros_like(seg_tr)
+
+    def is_pollution(p):
+        return True if cat_data.ix[p - 1].fi / cat_data.ix[p - 1].fp < 2.5 or abs(p-gal_flag) > 1 else False
+    for i in np.arange(poll_tr.shape[0]):
+        for j in np.arange(poll_tr.shape[1]):
+            s = seg_tr[i][j]
             if s and s != gal_flag:
-                pollutoin[i][j] = 1 if cat_data.ix[s - 1].fi / cat_data.ix[s - 1].fp < 2.5 else 0
-    belong_to_galaxy = mph.remove_small_objects(np.where((abs(seg_data - gal_flag) < 2) & (pollutoin == 0) & (seg_data != 0), one, zero))
-    y, x = np.where(belong_to_galaxy)
-    dist = np.sqrt((y - py) ** 2 + (x - px) ** 2)
-    radius = min(150, np.max(dist))
-    return tr(init_data, py, px, radius), tr(seg_data, py, px, radius), tr(belong_to_galaxy, py, px, radius), tr(pollutoin, py, px, radius)
+                poll_tr[i][j] = poll_tr[-i-1][-j-1] = 1 if is_pollution(s) else 0
+    belong_to_galaxy = mph.remove_small_objects(np.where((abs(seg_tr - gal_flag) < 2) & (poll_tr == 0) & (seg_tr != 0), np.ones_like(seg_tr, dtype=bool), np.zeros_like(seg_tr, dtype=bool)),
+                                                connectivity=2, min_size=100)
+    # plt.imshow(belong_to_galaxy)
+    # plt.show()
+    return init_tr, seg_tr, belong_to_galaxy, poll_tr
 
 
 def display():
@@ -68,7 +75,6 @@ def display():
 
 def seg2mask(segmentation, pollution):
     sy, sx = segmentation.shape
-
     flag = segmentation[sy / 2][sx / 2]
     one = np.ones_like(segmentation)
     zero = np.zeros_like(segmentation)
@@ -84,6 +90,7 @@ def inpainting(image, mask_map):
 def cal_ay(image, belong_to_galaxy):
     zero = np.zeros_like(image)
     I = np.where(belong_to_galaxy, image, zero)
+    plt.imsave('/Users/franky/Desktop/templates/init/1/%s.jpg' % fit, -log(I), cmap='gray', dpi=400)
     I180 = np.rot90(I, 2)
     return np.sum(np.abs(I - I180)) * 0.5 / np.sum(np.abs(I))
 
@@ -91,15 +98,15 @@ def cal_ay(image, belong_to_galaxy):
 if __name__ == '__main__':
     fits = pd.read_csv('list.csv')
     pro = 0
-    for fit in fits[fits.Z1 < 0.05]['NAME2'].values:
+    for fit in fits[fits.Z1 < 0.05]['NAME1'].values[:1]:
         print(fit, end='    ')
-        cy, cx, init, seg, cat = load(2, fit)
+        # fit = 'J114433.07+613200.7'
+        cy, cx, init, seg, cat = load(1, fit)
         img, sg, btg, poll = truncate(init, seg, cat, cy, cx)
-        inpaint_img = inpainting(img, seg2mask(sg, poll))
-        plt.imsave('/Users/franky/Desktop/templates/init/%s.jpg' % fit, log(inpaint_img), cmap='gray', dpi=200)
-        fits.at[pro, 'INIT'] = cal_ay(inpaint_img, btg)
+        # inpaint_img = inpainting(img, seg2mask(sg, poll))
+        # plt.imsave('/Users/franky/Desktop/templates/init/%s.jpg' % fit, -adapt_hist(img), cmap='gray', dpi=200)
+        fits.at[pro, 'A'] = cal_ay(img, btg)
+        print(cal_ay(img, btg), end='   ')
         print(pro)
         pro += 1
-    fits.to_csv('init2.csv', index=None, columns=['NAME2', 'INIT'])
-    # plt.imshow(-inpainting(norm(img), seg2mask(sg)), cmap='gray')
-    # plt.show()
+        fits.to_csv('init1.csv', index=None, columns=['NAME1', 'A'])
