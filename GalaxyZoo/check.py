400b866f1b3320ec49887e81d1592d81adb3163c
fmxustc <fmxustc@mail.ustc.edu.cn>
2016-09-18 02:07:27 -0700

    hh

--- GalaxyZoo/check.py
+++ GalaxyZoo/check.py
@@ -1,19 +1,43 @@
 import seaborn as sns
 import pandas as pd
+import matplotlib.pyplot as plt
 import termcolor
 import numpy as np
 from astropy.coordinates import SkyCoord as sky
 import astropy.units as unit
 
 
-def feature_farction(feature,sample):
+def selection(feature, sample):
+    if feature == 'bar':
+        return sample[(sample.t01_smooth_or_features_a02_features_or_disk_fraction > 0.227) &
+                      (sample.t02_edgeon_a05_no_fraction > 0.519) &
+                      (sample.t03_bar_a06_bar_count > 10) &
+                      (sample.t03_bar_a06_bar_fraction > 0.3)]
+    elif feature == 'spiral':
+        return sample[(sample.t01_smooth_or_features_a02_features_or_disk_fraction > 0.227) &
+                      (sample.t02_edgeon_a05_no_fraction > 0.519) &
+                      (sample.t04_spiral_a08_spiral_count > 10) &
+                      (sample.t04_spiral_a08_spiral_fraction > 0.3)]
+    elif feature == 'odd':
+        return sample[(sample.t06_odd_a14_yes_count > 10) &
+                      (sample.t06_odd_a14_yes_fraction > 0.3)]
+
+
+def feature_farction(sample):
     sep = 0.01
     for z in np.arange(0, 0.2, sep):
         whole = sample[(sample.z <= z + sep) & (sample.z > z)]
         whole = whole.sort_values(by='Mr')
         whole.index = range(len(whole))
-        pass
-        # for m in np.arange(-19,-21,-0.5)
+        complete_index = whole[whole.mr > 17].index[0] - 1
+        print(complete_index)
+        com_set = whole.ix[:complete_index]
+        for fea in features:
+            reset_index = gal[(gal.z == z) & (gal.feature == fea)].index[0]
+            gal.at[reset_index, 'Mr'] = whole.ix[complete_index, 'Mr']
+            gal.at[reset_index, 'fraction'] = len(selection(fea, com_set)) / len(com_set)
+    print(gal)
+    gal.to_csv('gal.csv', index=False)
 
 
 zoo = pd.read_csv('zoo2MainSpecz.csv', header=0)
@@ -22,8 +46,16 @@ zoo['Mr'] = uni.ix[zoo.dr7objid.values].PETROMAG_MR.values
 zoo['mr'] = uni.ix[zoo.dr7objid.values].PETROMAG_R.values
 zoo['z'] = uni.ix[zoo.dr7objid.values].REDSHIFT.values
 # z, Mr, mr
-gal = pd.DataFrame(columns=['z','Mr','fraction','feature'])
-gal['z'] = np.repeat(np.arange(0,0.2,0.01),4)
-gal['Mr'] = np.tile(np.arange(-19,-21,-0.5),20)
-gal['featrue'] = np.tile(['bar','bulge','odd','spiral'],20)
-print(gal)
+gal = pd.DataFrame(columns=['z', 'Mr', 'fraction', 'feature'])
+
+features = ['bar', 'odd', 'spiral']
+
+gal['z'] = np.repeat(np.arange(0, 0.2, 0.01), 3)
+gal['feature'] = np.tile(features, 20)
+# gal.index = gal.z
+
+# print(gal)
+feature_farction(zoo)
+f = sns.FacetGrid(gal,col='feature',sharey=False,xlim=[0,0.2])
+f.map(sns.tsplot, 'z','fraction')
+plt.show()
--- GalaxyZoo/match.py
+++ GalaxyZoo/match.py
@@ -4,6 +4,7 @@ import numpy as np
 from astropy.coordinates import SkyCoord as sky
 import astropy.units as unit
 import seaborn as sns
+import matplotlib.pyplot as plt
 
 
 def match(sample_ra, sample_dec, catalog_ra, catalog_dec):
@@ -20,6 +21,10 @@ def select_bar(sample):
                   (sample.t02_edgeon_a05_no_fraction >= 0.519) &
                   (sample.t03_bar_a06_bar_count >= 10) &
                   (sample.t03_bar_a06_bar_fraction > 0.3)]
+    # return sample[(sample.t01_smooth_or_features_a02_features_or_disk_fraction > 0.227) &
+    #                   (sample.t02_edgeon_a05_no_fraction > 0.519) &
+    #                   (sample.t04_spiral_a08_spiral_count > 10) &
+    #                   (sample.t04_spiral_a08_spiral_fraction > 0.3)]
 
 
 def check_mag(sample):
@@ -28,22 +33,29 @@ def check_mag(sample):
         # 从 z 到 z+0.01的区间
         whole = sample[(sample.z <= z+sep) & (sample.z > z)]
         whole = whole.sort_values(by='Mr')
+        # print(whole)
         whole.index = range(len(whole))
         bar_index = whole[whole.mr > 17].index[0]
         barred_sample = select_bar(whole.ix[:bar_index-1])
-        print(len(barred_sample)/bar_index, whole.ix[bar_index-1, 'Mr'])
+        f.append(len(barred_sample)/bar_index)
+        print(bar_index, len(barred_sample), len(barred_sample)/bar_index)
+        # print(len(barred_sample)/bar_index, whole.ix[bar_index-1, 'Mr'])
 
-print(sns.load_dataset('titanic'))
-# one = pd.read_table('sdssDR4_type1_galaxy.txt', header=0, sep='\s+', engine='python')
-# # one = one[one.Z<0.05]
-# two = pd.read_table('sdssDR4_type2_galaxy.txt', header=0, sep='\s+', engine='python')
-# # two = two[two.Z<0.05]
-# zoo = pd.read_csv('zoo2MainSpecz.csv', header=0)
-#
-# # 预处理,在 Main Sample 加上红移和绝对星等
-# uni = pd.read_csv('gz2sample.csv', header=0, index_col='OBJID')
-# zoo['Mr'] = uni.ix[zoo.dr7objid.values].PETROMAG_MR.values
-# zoo['mr'] = uni.ix[zoo.dr7objid.values].PETROMAG_R.values
-# zoo['z'] = uni.ix[zoo.dr7objid.values].REDSHIFT.values
-# check_mag(zoo)
+# print(sns.load_dataset('titanic'))
+one = pd.read_table('sdssDR4_type1_galaxy.txt', header=0, sep='\s+', engine='python')
+# one = one[one.Z<0.05]
+two = pd.read_table('sdssDR4_type2_galaxy.txt', header=0, sep='\s+', engine='python')
+# two = two[two.Z<0.05]
+zoo = pd.read_csv('zoo2MainSpecz.csv', header=0)
+
+# 预处理,在 Main Sample 加上红移和绝对星等
+uni = pd.read_csv('gz2sample.csv', header=0, index_col='OBJID')
+zoo['Mr'] = uni.ix[zoo.dr7objid.values].PETROMAG_MR.values
+zoo['mr'] = uni.ix[zoo.dr7objid.values].PETROMAG_R.values
+zoo['z'] = uni.ix[zoo.dr7objid.values].REDSHIFT.values
+f = []
+check_mag(zoo)
+sns.tsplot(f)
+plt.show()
+
 
