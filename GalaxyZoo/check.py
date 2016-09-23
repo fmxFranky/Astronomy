0e88d744a7e31d47692ee887c8b2ec6e0ae910a0
fmxustc <fmxustc@mail.ustc.edu.cn>
2016-09-23 09:39:09 +0800

    check the full catalog of GZ2 whether have sample selection effect

--- GalaxyZoo/check.py
+++ GalaxyZoo/check.py
@@ -5,27 +5,28 @@ import termcolor
 import numpy as np
 from astropy.coordinates import SkyCoord as sky
 import astropy.units as unit
-
-
-def selection(feature, sample):
-    if feature == 'bar':
-        return sample[(sample.t01_smooth_or_features_a02_features_or_disk_fraction > 0.227) &
-                      (sample.t02_edgeon_a05_no_fraction > 0.519) &
-                      (sample.t03_bar_a06_bar_count > 10) &
-                      (sample.t03_bar_a06_bar_fraction > 0.3)]
+import astropy.cosmology as COS
+from astropy.cosmology import LambdaCDM
+import warnings
 
 
 def calculate_fraction():
-    for z in z_range:
-        whole = zoo[(zoo.z > z) & (zoo.z < z+0.01) & (zoo.Mr < -20.89) & (zoo.mr < 17)]
-        bar = whole[(whole.t01_smooth_or_features_a02_features_or_disk_fraction > 0.430) &
-                    (whole.t02_edgeon_a05_no_fraction > 0.715) &
-                    (whole.t02_edgeon_a05_no_count >= 20)]
-        result = bar[bar.t03_bar_a06_bar_fraction >= 0.8]
-        rid = gal[(gal.Mr == -20.89) & (gal.z == z) & (gal.root == 'bar')].index[0]
-        gal.at[rid,'fraction'] = len(result)/len(bar)
-    gal.to_csv('gal.csv', index=False)
+    for m in mag_range:
+        for z in z_range:
+            whole = zoo[(zoo.z > z) & (zoo.z < z + 0.01) & (zoo.Mr < m) & (zoo.mr < 17)]
+            bar = whole[(whole.t01_smooth_or_features_a02_features_or_disk_fraction > 0.430) &
+                        (whole.t02_edgeon_a05_no_fraction > 0.715) &
+                        (whole.t02_edgeon_a05_no_count >= 20)]
+            result = bar[bar['gz2class'].str.contains('SB')]
+            rid = gal[(gal.Mr == m) & (gal.z == z) & (gal.root == 'bar')].index[0]
+            wid = gal[(gal.Mr == m) & (gal.z == z) & (gal.root == 'whole')].index[0]
+            gal.at[rid, 'fraction'] = len(result) / len(bar)
+            gal.at[wid, 'fraction'] = len(result) / len(whole)
+    gal.to_csv('gal2.csv', index=False)
 
+
+warnings.filterwarnings('ignore')
+flatui = ["#9b59b6", "#3498db", "#95a5a6", "#e74c3c", "#34495e", "#2ecc71"]
 
 zoo = pd.read_csv('zoo2MainSpecz.csv', header=0)
 uni = pd.read_csv('gz2sample.csv', header=0, index_col='OBJID')
@@ -38,7 +39,7 @@ gal = pd.DataFrame(columns=['z', 'Mr', 'fraction', 'feature', 'root'])
 features = ['bar', 'odd', 'spiral']
 z_range = np.arange(0.01, 0.2, 0.01)
 nz = len(z_range)
-mag_range = np.arange(-19.89, -21.89, -0.5)
+mag_range = np.arange(-18.89, -21.89, -0.5)
 nm = len(mag_range)
 root_range = ['whole', 'bar']
 nr = len(root_range)
@@ -47,5 +48,18 @@ gal['Mr'] = np.tile(np.repeat(mag_range, nr), np.array(nz))
 gal['root'] = np.tile(root_range, np.array(nz * nm))
 
 calculate_fraction()
-plt.plot(z_range,gal[(gal.Mr == -20.89) & (gal.root == 'bar')]['fraction'].values)
+redshift_cut = [COS.z_at_value(LambdaCDM(H0=70, Om0=0.3, Ode0=0.7).distmod, (17 - mag) * unit.mag)
+                for mag in mag_range]
+gal = pd.read_csv('gal.csv')
+g = sns.FacetGrid(gal, hue='root', col='Mr', col_wrap=3, col_order=mag_range, palette=flatui)
+g.map(plt.plot, 'z', 'fraction')
+g.add_legend()
+g.set(ylim=(0, .5))
+i = 0
+for ax in g.axes.ravel():
+    ax.plot([redshift_cut[i],redshift_cut[i]],[0,1],'k--')
+    ax.text(redshift_cut[i]+0.01,0.45,'z=%f'% redshift_cut[i],
+            fontsize=10,bbox=dict(facecolor='#95a5a6', alpha=0.4))
+    i += 1
+
 plt.show()
